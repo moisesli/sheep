11c82fceb01ebe4d931a539e170f30913312a906
moises <elnaufrago2009@hotmail.com>
2014-04-02 18:52:01 +0200

    app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weperu::Application.config.secret_key_base = 'dc5d4b8f8f4ef574dbee2e2ad6423d35cc00f1bc0ef775ef36af0cb0738abe73064043fdfb4e9c4d8d1fdf7cb1319cada36b6c3cb8bb1ddf4542efb1aa924450'
